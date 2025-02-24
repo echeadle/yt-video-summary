50679e78ca3cb56b0c29432a6592456a0f5622eb
echeadle <echeadle@gmail.com>
2025-02-24 11:11:10 -0700

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# yt-video-summary
+# YouTube Video Summarizer/Translator
+
+## How to Install
+
+1. git clone https://github.com/tylerprogramming/yt-video-summary.git
+2. `cd yt-video-summary`
+3. type `pip install -r requirements.txt` in terminal
+4. add your YouTube URL into *main.py*
+5. add your openai key to the .env property
+6. run `python main.py`
+
 YouTube Video: Let AI Work For You  Tyler AI
---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+
+from youtube.YouTubeAudioDownloader import YouTubeAudioDownloader
+from youtube.AudioTranscriber import AudioTranscriber
+from youtube.AutoGenInteraction import AutoGenInteraction
+
+load_dotenv()
+
+
+def main():
+    url = "https://www.youtube.com/watch?v=PUPO2tTyPOo&t=7s"
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+
+        # gives you the filename from yt video
+    downloader = YouTubeAudioDownloader(url)
+    filename = downloader.download_audio()
+    print(f"Downloaded and saved audio (m4a) as: {filename}")
+
+    # transcribes the audio
+    transcriber = AudioTranscriber(api_key=openai_api_key)
+    transcription = transcriber.transcribe_audio(filename)
+    print("Transcription completed.")
+
+    # ai agents summarize/translate
+    autogen_interaction = AutoGenInteraction(api_key=openai_api_key, transcription=transcription)
+    autogen_interaction.initiate_chat()
+    print("Chat completed.")
+
+
+if __name__ == "__main__":
+    main()
